22f906f14c92bd703b8c46f36db0a4be213521fa
Alibek314 <105502545+Alibek314@users.noreply.github.com>
2023-09-14 18:19:45 +0600

    Added table-sort function

--- EmployeesApp/views.py
+++ EmployeesApp/views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponse
 from .models import Employees
 from django.views.generic import ListView
 from django.http import JsonResponse
@@ -29,18 +29,26 @@ def show_team(request):
     team = Employees.objects.all()
     paginator = Paginator(team, 100)
 
-    if is_ajax(request):
-        sort_by = request.GET["sort_by"]
-        page_num = request.GET["page_number"]
-        first_index = (page_num * 100) - 100
-        last_index = (page_num * 100) + 1
-        sorted_employees = Employees.objects.order_by(sort_by)[first_index:last_index]
-        return render(request, "EmployeesApp/show_team.html", {
-            "page_obj": sorted_employees,
+    if "Table-Order" in request.headers:
+        sort_by = request.headers["Table-Order"].split(",")[1]
+        page_num = int(request.headers["Table-Order"].split(",")[0])
+        current_objects = paginator.get_page(page_num).object_list
+        if sort_by == 'name':
+            sorted_employees = sorted(current_objects, key=lambda x: x.name)
+        elif sort_by == 'position':
+            sorted_employees = sorted(current_objects, key=lambda x: x.position)
+        elif sort_by == 'hire_date':
+            sorted_employees = sorted(current_objects, key=lambda x: x.hire_date)
+        elif sort_by == 'salary':
+            sorted_employees = sorted(current_objects, key=lambda x: x.salary)
+        else:
+            sorted_employees = current_objects
+        return render(request, "EmployeesApp/sorted_table.html", {
+            "sorted_employees": sorted_employees,
         })
+
     else:
-        page_number = request.GET.get("page")
-        page_obj = paginator.get_page(page_number)
+        page_obj = paginator.get_page(request.GET.get("page"))
         return render(request, "EmployeesApp/show_team.html", {
                     "page_obj": page_obj
             })
